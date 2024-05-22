3825d7162136db2ba6a4bc4090200525325d7cab
Andre Markov <andremarkov@icloud.com>
2024-05-22 17:35:42 +0300

    add gemini library

--- app/Jobs/ProcessReceipt.php
+++ app/Jobs/ProcessReceipt.php
@@ -8,6 +8,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use GeminiAPI\Laravel\Facades\Gemini;
+use Exception;
 
 class ProcessReceipt implements ShouldQueue
 {
@@ -22,8 +24,25 @@ class ProcessReceipt implements ShouldQueue
 
     public function handle()
     {
-        // TODO Реальная логика обработки чека
-        $this->receipt->processed = true;
-        $this->receipt->save();
+        $prompt = 'Это чек?';
+        try {
+            $response = Gemini::generateTextUsingImage(
+                'image/jpeg',
+                base64_encode(file_get_contents(storage_path('app/public/receipts/' . $this->receipt->image_path))),
+                $prompt,
+            );
+
+            dd($response);
+
+            // TODO Реальная логика обработки чека
+            // https://github.com/gemini-api-php/laravel
+            $this->receipt->processed = true;
+            $this->receipt->save();
+        } catch (Exception $e) {
+            logger('Error processing receipt: ' . $e->getMessage());
+
+            $this->receipt->error = true;
+            $this->receipt->save();
+        }
     }
 }
