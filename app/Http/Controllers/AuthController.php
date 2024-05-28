2e35fc33df339e776881d57d7347461b2e73c092
Andre Markov <andremarkov@icloud.com>
2024-05-28 17:48:42 +0300

    cors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -135,4 +135,9 @@ class AuthController extends Controller
 
         return response()->json(['message' => 'Logged out successfully']);
     }
+
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
 });
 
 Route::get('hello', function () {
