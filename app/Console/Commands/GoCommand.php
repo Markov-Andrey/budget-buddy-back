138157bf54d5b2b7d6fad401dd41642c0c222328
Markov Andrey <andremarkov@icloud.com>
2024-06-28 22:48:48 +0300

    add Go command

---
+++ app/Console/Commands/GoCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GoCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'go';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Process discord and execute queue';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('queue:discord');
+        $this->call('queue:work');
+
+        $this->info('All commands have been executed.');
+
+        return 0;
+    }
+}
